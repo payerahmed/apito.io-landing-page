94af136d2f3e8b40a6cd22d1a9f2512b1f28d184
payerahmed <mdpayer125@gmail.com>
2021-06-09 01:15:55 +0600

    add some functionality

--- src/component/client/Client.js
+++ src/component/client/Client.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import star from '../../img/star.png';
 import cone from '../../img/cone.png';
 import ctwo from '../../img/ctwo.png';
@@ -8,55 +8,75 @@ import arrowleft from '../../img/arrwleft.png';
 import arrwright from '../../img/arrwright.png';
 
 import './Client.css';
-
+var clientArr = [
+  {
+    star: star,
+    isActive: true,
+    position: 'Modern look & trending design',
+    detailes:
+      'Get working experience to work with this amazing team & in future want to work together for bright future projects and also make deposit to freelancer.',
+    clientImg: cone,
+    clientName: 'Denny Hilguston',
+    clientEmail: '@denny.hil'
+  },
+  {
+    star: star,
+    isActive: false,
+    position: 'Design Quality & performance',
+    detailes:
+      'Get working experience to work with this amazing team & in future want to work together for bright future projects and also make deposit to freelancer.',
+    clientImg: ctwo,
+    clientName: 'Denny Hilguston',
+    clientEmail: '@denny.hil'
+  },
+  {
+    star: star,
+    isActive: false,
+    position: 'Layout and organized layers',
+    detailes:
+      'Get working experience to work with this amazing team & in future want to work together for bright future projects and also make deposit to freelancer.',
+    clientImg: cthree,
+    clientName: 'Denny Hilguston',
+    clientEmail: '@denny.hil'
+  },
+  {
+    star: star,
+    isActive: false,
+    position: 'Modern look & trending design',
+    detailes:
+      'Get working experience to work with this amazing team & in future want to work together for bright future projects and also make deposit to freelancer.',
+    clientImg: cfour,
+    clientName: 'Denny Hilguston',
+    clientEmail: '@denny.hil'
+  },
+  {
+    isActive: false,
+    star: star,
+    position: 'Modern look & trending design',
+    detailes:
+      'Get working experience to work with this amazing team & in future want to work together for bright future projects and also make deposit to freelancer.',
+    clientImg: cfour,
+    clientName: 'Denny Hilguston',
+    clientEmail: '@denny.hil'
+  }
+];
 const Client = () => {
-  const clientArr = [
-    {
-      star: star,
-      position: 'Modern look & trending design',
-      detailes:
-        'Get working experience to work with this amazing team & in future want to work together for bright future projects and also make deposit to freelancer.',
-      clientImg: cone,
-      clientName: 'Denny Hilguston',
-      clientEmail: '@denny.hil'
-    },
-    {
-      star: star,
-      position: 'Design Quality & performance',
-      detailes:
-        'Get working experience to work with this amazing team & in future want to work together for bright future projects and also make deposit to freelancer.',
-      clientImg: ctwo,
-      clientName: 'Denny Hilguston',
-      clientEmail: '@denny.hil'
-    },
-    {
-      star: star,
-      position: 'Layout and organized layers',
-      detailes:
-        'Get working experience to work with this amazing team & in future want to work together for bright future projects and also make deposit to freelancer.',
-      clientImg: cthree,
-      clientName: 'Denny Hilguston',
-      clientEmail: '@denny.hil'
-    },
-    {
-      star: star,
-      position: 'Modern look & trending design',
-      detailes:
-        'Get working experience to work with this amazing team & in future want to work together for bright future projects and also make deposit to freelancer.',
-      clientImg: cfour,
-      clientName: 'Denny Hilguston',
-      clientEmail: '@denny.hil'
-    },
-    {
-      star: star,
-      position: 'Modern look & trending design',
-      detailes:
-        'Get working experience to work with this amazing team & in future want to work together for bright future projects and also make deposit to freelancer.',
-      clientImg: cfour,
-      clientName: 'Denny Hilguston',
-      clientEmail: '@denny.hil'
-    }
-  ];
+  const [arr, setArr] = useState([]);
+  useEffect(() => {
+    setArr(clientArr);
+  }, []);
+
+  const cardHover = (e) => () => {
+    var clientArrcopy = arr.slice();
+    clientArrcopy.map((e, i) => {
+      if (e.isActive) {
+        clientArrcopy.splice(i, 1, { ...e, isActive: false });
+      }
+    });
+    var activeCard = clientArr[e];
+    clientArrcopy.splice(e, 1, { ...activeCard, isActive: true });
+    setArr(clientArrcopy);
+  };
 
   return (
     <div>
@@ -64,9 +84,16 @@ const Client = () => {
       <div className='section__heading'>Meet Client Satisfaction</div>
       <div className='client__card--container'>
         <div className='client__cards'>
-          {clientArr.map((e, indx) => {
+          {arr.map((e, indx) => {
             return (
-              <div className='client__card'>
+              <div
+                className={
+                  e.isActive
+                    ? 'client__card client__card--active'
+                    : 'client__card'
+                }
+                onMouseOver={cardHover(indx)}
+              >
                 <img src={e.star} alt='' />
                 <div className='position'>{e.position}</div>
                 <div className='detailes'>{e.detailes}</div>
--- src/component/query/Query.js
+++ src/component/query/Query.js
@@ -1,41 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Query.css';
 import queryImg from '../../img/code.png';
+var quaryArr = [
+  { isActive: true, name: 'Wildcard Search on All Records' },
+  { isActive: false, name: 'Filtering and Sorting' },
+  { isActive: false, name: 'Filtering and Sorting' },
+  { isActive: false, name: 'Complex Nested Filtering' },
+  { isActive: false, name: 'Geo Location Search' },
+  { isActive: false, name: 'Custom Functions' },
+  { isActive: false, name: 'Records with Pagination' },
+  { isActive: false, name: 'Login or Signup' }
+];
 const Query = () => {
+const [arr, setArr] = useState([])
+
+useEffect(()=> {
+  setArr(quaryArr)
+}, [])
+  const over = e => () => {
+    var quaryArrCopy = arr.slice()
+    quaryArrCopy.map((e,i) => {
+      if(e.isActive) {
+        quaryArrCopy.splice(i, 1, {...e, isActive: false})
+      }
+    })
+    var target = quaryArr[e]
+    quaryArrCopy.splice(e, 1,{...target, isActive: true})
+    setArr(quaryArrCopy)
+  }
   return (
     <div>
       <div className='section__text'>QUERY Patterns</div>
       <div className='section__heading'>
         Perform Complex Queries and Write Custom Functions
       </div>
-      <div className='perfom__container'>
+      <div className='container'>
         <div className='flex'>
-          <div className='perfom__container--left'>
-            <div className='left__text'>
-              <span>Wildcard Search on All Records</span>
-            </div>
-            <div className='left__text'>
-              <span>Filtering and Sorting</span>
-            </div>
-            <div className='left__text'>
-              <span>Filtering and Sorting</span>
-            </div>
-            <div className='left__text'>
-              <span>Complex Nested Filtering</span>
-            </div>
-            <div className='left__text'>
-              <span>Geo Location Search</span>
-            </div>
-            <div className='left__text'>
-              <span>Custom Functions</span>
-            </div>
-            <div className='left__text'>
-              <span>Records with Pagination</span>
-            </div>
-            <div className='left__text'>
-              <span>Login or Signup</span>
-            </div>
+          <div className='perfom__container'>
+            {arr.map((e, indx) => {
+              return (
+                <div className='perfom__container--left' key={indx}>
+                  <div
+                    className={
+                      e.isActive
+                        ? 'left__text left__text--active'
+                        : 'left__text'
+                    }
+                    onMouseOver={over(indx)}
+                  >
+                    <div>{e.name}</div>
+                  </div>
+                </div>
+              );
+            })}
           </div>
+
           <div className='perfom__container--right'>
             <img src={queryImg} alt='' />
           </div>
